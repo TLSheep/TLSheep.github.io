463b7105a68c647b40e3126b40a7720f76876646
TlessSheep <626709960@qq.com>
2024-08-19 23:54:54 +0800

    添加完整项目

---
+++ src/components/Project5.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="container">
+    <!-- <img class="pic" src="../../public/imgs/2.jpg" > -->
+    <img class="pic" :src="props.pic_src" >
+    <div class="mask" :style="{ 'backdrop-filter': `blur(${40-count/2.5}px)` }"></div>
+    <div class="count_down" :style="{opacity:1-count/100}">{{count>95?99:count+5}}%</div>
+  </div>
+</template>
+<script setup>
+  import {ref,defineProps} from 'vue'
+  const count = ref(0);
+  const props=defineProps({
+      pic_src:{
+        type:String,
+        default:'/src/assets/imgs/6.png'
+      },
+      speed:{
+        type:Number,
+        default:30
+      }
+  })
+function countDown(){
+    let num = 0;
+    let timer = setInterval(()=>{
+        num++;
+        if(num===100){
+            clearInterval(timer);
+        }
+        count.value = num;
+    },props.speed)
+}
+countDown()
+</script>
+<style scoped>
+*{
+    box-sizing: border-box;
+}
+.container{
+    height: 100vh;
+    width: 100vw;
+  
+    overflow: hidden;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+.pic {
+    position: relative;
+    max-height: 150vh;
+    max-width: 150vw;
+    min-height: 100%;
+    /* min-width: 100%; */
+    top: 0;
+    left: 0;
+    object-fit: fill;
+    
+}
+.mask{
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    z-index: 1;
+}
+
+.count_down {
+    position: absolute;
+    top: 50%-50px  ;
+    font-size: 50px;
+    color: #fff;
+    z-index: 100;
+}
+</style>
---
+++ src/components/Project7.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="container">
+    <div class="small_container left" >
+        <img class="pic" src="/src/assets/imgs/1.png">
+        <div class="items">
+            <div class="title">Left</div>
+            <div class="btn">S G</div>
+        </div>
+    </div>
+    <div class="small_container right" >
+        <img class="pic" src="/src/assets/imgs/2.png">
+        <div class="items">
+            <div class="title">Right</div>
+            <div class="btn">M M</div>
+        </div>
+    </div>
+  </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+*{
+    box-sizing: border-box;
+}
+.container{
+    display: flex;
+    min-height: 100vh;
+    min-width: 100vw;
+}
+
+.small_container{
+    flex: 1;
+	display: flex;
+    justify-content: center;
+    align-content: center;
+    position: relative;
+    overflow: hidden;
+    transition: ease-in-out 1s;
+}
+.small_container:hover{
+    flex: 3;
+}
+
+.left{
+    background-color: #7A78E2;
+}
+
+.right{
+    background-color: #555555;
+}
+
+.pic{
+    position: absolute;
+    max-height: 120vh;
+    max-width: 120vw;
+    min-height: 100vh;
+    min-width: 100vw;
+    z-index: 1;
+    opacity: 0.5;
+}
+.left .pic{
+    top:-15%;
+}
+.right .pic{
+    top:-10%;
+}
+.title{
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    font-size: 4rem;
+    color: #fff;
+    white-space: nowrap;
+}
+.items {
+	display: flex;
+    flex-direction: column;
+    align-content: center;
+    justify-content: center;
+    transform: translateY(-20%);
+    z-index: 100;
+}
+
+.btn{
+   	display: flex;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    color: #fff;
+    border: #fff solid 0.2rem;
+    font-size: 20px;
+    font-weight: bold;
+    text-transform: uppercase;
+    width: 200px;
+    padding: 25px;
+    margin-top: 50px;
+    border-radius: 25px;
+    transition: 0.3;
+    border-radius: 20px;
+    cursor: pointer;
+    transition:  0.15s;
+}
+
+.left div[class*="btn"]:hover{
+    background-color: #5754EC;
+    border-color: #5754EC;
+}
+
+.right .btn:hover{
+    background-color: #1C7A1C;
+    border-color: #1C7A1C;
+}
+</style>
